a98c58bdddb74e2f9c8ea439f09f6840193b1413
Julia Diggs <juliadiggs@julias-mbp.wireless.cougars.int>
2018-10-25 18:43:20 -0400

    First Commit

--- action.js
+++ action.js
@@ -7,9 +7,45 @@ function checkEmail(email) {
 function checkAge() {
     var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
     
-    if() {
-       
+    if(document.getElementById("age").value == "." || document.getElementById("age").value > 2) {
+       document.getElementById("age").style.backgroundColor = "red";
     } else {
-       
+       document.getElementById("age").style.backgroundColor = "white";
     }
 }
+
+
+//name.addEventListener("onkeypress", checkName());
+document.getElementById("name").onkeyup = function checkName() {
+    var name = document.getElementById("name");
+    var regExp = new RegExp('<');
+
+    if(regExp.test(document.getElementById("name").value)){
+        //turn background red
+        //document.getElementById("name").style.backgroundColor = 'red';
+        name.style.backgroundColor = 'red';
+    }else{
+        //turn background white
+        //document.getElementById("name").style.backgroundColor = ' ';
+        name.style.backgroundColor = 'white';
+    }
+
+}
+
+/*
+function checkName() {
+
+    var regExp = new RegExp('<');
+
+    if(regExp.test(document.getElementById("name").value)){
+        //turn background red
+        document.getElementById("name").style.backgroundColor = 'red';
+    }else{
+        //turn background white
+        document.getElementById("name").style.backgroundColor = ' ';
+
+    }
+
+
+
+}*/
