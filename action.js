aebd146c1556735dcc4902a1e7c43e1a18f3e82d
Julia Diggs <juliadiggs@Julias-MacBook-Pro.local>
2018-10-29 19:58:34 -0400

    Second Commit

--- action.js
+++ action.js
@@ -1,21 +1,31 @@
 function checkEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    var regExp = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');
+
+
+
 
 }
 
-function checkAge() {
-    var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
+document.getElementById("age").onkeyup = function checkAge() {
+    var regExp = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
+    var age = document.getElementById("age");
     
-    if(document.getElementById("age").value == "." || document.getElementById("age").value > 2) {
-       document.getElementById("age").style.backgroundColor = "red";
+    if(document.getElementById("age").value === "." || document.getElementById("age").value > 2) {
+       age.style.backgroundColor = "red";
     } else {
-       document.getElementById("age").style.backgroundColor = "white";
+       age.style.backgroundColor = "white";
     }
+ /*   if(regExp.test(document.getElementById("age").contains("."))){
+        age.style.backgroundColor = 'red';
+
+    }else{
+        age.style.backgroundColor = 'white';
+    }
+*/
 }
 
 
-//name.addEventListener("onkeypress", checkName());
+
 document.getElementById("name").onkeyup = function checkName() {
     var name = document.getElementById("name");
     var regExp = new RegExp('<');
@@ -32,20 +42,3 @@ document.getElementById("name").onkeyup = function checkName() {
 
 }
 
-/*
-function checkName() {
-
-    var regExp = new RegExp('<');
-
-    if(regExp.test(document.getElementById("name").value)){
-        //turn background red
-        document.getElementById("name").style.backgroundColor = 'red';
-    }else{
-        //turn background white
-        document.getElementById("name").style.backgroundColor = ' ';
-
-    }
-
-
-
-}*/
