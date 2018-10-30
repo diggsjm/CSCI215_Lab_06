e039b044692d3648ec3d28c1ddd56b0d7455466f
Julia Diggs <juliadiggs@julias-mbp.wireless.cougars.int>
2018-10-30 15:47:23 -0400

    Third Commit

--- action.js
+++ action.js
@@ -1,20 +1,62 @@
+document.getElementById("subscribe").onclick = function validate() {
+    checkEmail();
+
+}
+
+
+
 function checkEmail(email) {
     var regExp = new RegExp('[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?');
+    var email = document.getElementById("email");
 
+    if(regExp.test(email) == true){
+        //if this returns true call on myAjaxFunction()
+        //inside myAjaxFunction--- have an alert pop up and clear the data fields
+        myAjaxFunction();
+    }else{
+        //returns false -- alert the subscription did not work
+        alert("Subscription couldn't be processed");
+    }
 
+}
+
+function myAjaxFunction(){
+    alert("Ajax has been made");
+
+    document.getElementsByClassName("subform").reset();
 
 
 }
 
+
 document.getElementById("age").onkeyup = function checkAge() {
-    var regExp = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
+//    var regExp = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
+    var regExp = new RegExp('[0,9]{1,3}');
     var age = document.getElementById("age");
-    
-    if(document.getElementById("age").value === "." || document.getElementById("age").value > 2) {
+    console.log("Age is running");
+
+    for(var i = 0; i <age; i++){
+        if(regExp.test(document.getElementById("age").valueOf(i) === ".") || regExp.test(document.getElementById("age").length > 2)){
+            age.style.backgroundColor = 'red';
+            console.log("Length is larger than 2");
+        }else{
+            age.style.backgroundColor = 'white';
+        }
+    }
+
+/*    if(regExp.test(document.getElementById("age").length > 2)){
+        age.style.backgroundColor = 'red';
+        console.log("Length is larger than 2");
+    }else{
+        age.style.backgroundColor = 'white';
+    }*/
+//regExp.test(document.getElementById("age").value === ".") ||
+
+/*    if(document.getElementById("age").value === "." || document.getElementById("age").value > 2) {
        age.style.backgroundColor = "red";
     } else {
        age.style.backgroundColor = "white";
-    }
+    }*/
  /*   if(regExp.test(document.getElementById("age").contains("."))){
         age.style.backgroundColor = 'red';
 
